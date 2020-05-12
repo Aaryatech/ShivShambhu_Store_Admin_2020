c2d4c31110a244ea5b342bbd3d1b019525c2b63f
Aaryatech <code@atsinfosoft.com>
2020-05-12 10:48:38 +0530

    Anmol

---
+++ src/main/java/com/ats/tril/common/Constants.java
@@ -0,0 +1,16 @@
+package com.ats.tril.common;
+
+public class Constants {
+
+	//public static final String url = "http://localhost:8094/";
+	public static final String url="http://192.169.205.160:8080/StoreApiDemo/";
+	public static final String ReportURL = "http://192.169.205.160:8080/ShivShambhuAdmin/";
+	public static final String Item_Image_URL = "http://192.169.205.160:8080/uploads/mongistoreuploads/";
+	public static String REPORT_SAVE = "apache-tomcat-8.5.40/webapps/uploads/shiv/Report.pdf";
+	public static final int ItemImage = 1;
+	public static int subAct = 0;
+	public static int mainAct = 0;
+	public static int delStatus = 1;
+	public static String adminUrl = "http://192.169.205.160:8080/ShivShambhuWebApi/";
+
+}
